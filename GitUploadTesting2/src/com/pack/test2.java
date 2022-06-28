540e03b89cce191dcc8a0eb2de902d75d53bf396
dell <dell@DESKTOP-M95PPV0>
2022-06-28 10:38:19 +0530

    testing done

---
+++ GitUploadTesting2/src/com/pack/test2.java
@@ -0,0 +1,12 @@
+package com.pack;
+
+public class test2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("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");
+		System.out.println("-------------------------");
+        System.out.println("testing is done");
+	}
+
+}

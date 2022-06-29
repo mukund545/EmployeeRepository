9fb0975db88441f7e33748836ab8a42101a746e6
dell <dell@DESKTOP-M95PPV0>
2022-06-29 16:20:08 +0530

    test added

---
+++ GitUploadTesting2/src/com/pack/Test.java
@@ -0,0 +1,15 @@
+package com.pack;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("29-06-2022");
+		
+		System.out.println(" ------");
+		
+	   System.out.println("testing is done today on git hub.");
+	}
+}
--- GitUploadTesting2/src/com/pack/test2.java
+++ GitUploadTesting2/src/com/pack/test2.java
@@ -1,12 +1,17 @@
 package com.pack;
 
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class test2 {
-	
+
 	public static void main(String[] args) {
 		
+
 		System.out.println("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");
 		System.out.println("-------------------------");
-        System.out.println("testing is done");
+		System.out.println("testing is done");
 	}
 
 }
